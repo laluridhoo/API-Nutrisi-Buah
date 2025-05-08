10f97396bf5af4c0fab4678b9a0ca43846663cd1
lalu kurnia muhammad ridho <nama_user@DESKTOP-SMPFNDV>
2025-05-08 17:48:05 +0800

    mengatur credential service

--- config/firestore.js
+++ config/firestore.js
@@ -1,14 +1,10 @@
 // firestore.js
 const admin = require('firebase-admin');
-const path = require('path');
 
-// Gunakan file service account
-const serviceAccount = require(path.resolve(__dirname, '/home/nama_user/keyService/smart-nutrition-app-457602-8a5427edbe99.json'));
-
-// Cegah inisialisasi ganda (penting saat di-deploy atau testing)
+// Inisialisasi Firebase Admin dengan credential dari env variable
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
     projectId: 'smart-nutrition-app-457602'
   });
 }
@@ -17,6 +13,5 @@ const db = admin.firestore();
 
 module.exports = {
   db,
-  admin,
-  serviceAccount, // kalau di handler butuh Timestamp misalnya
+  admin
 };
