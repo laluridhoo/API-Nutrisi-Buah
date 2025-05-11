0af93465b4e1514b6fc35e1c8ff928117a020cef
lalu kurnia muhammad ridho <nama_user@DESKTOP-SMPFNDV>
2025-05-11 16:38:03 +0800

    menambahkan foto diriwayat komsumsi harian

--- config/firestore.js
+++ config/firestore.js
@@ -1,12 +1,11 @@
 const admin = require('firebase-admin');
-const path = require('path');
+require('dotenv').config();
 
-// Load credentials from file
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const path = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(require(path)),
   });
 }
 
@@ -15,5 +14,5 @@ const db = admin.firestore();
 module.exports = {
   db,
   admin,
-  serviceAccount, // jika ingin akses info credential
+  serviceAccount: null,
 };
