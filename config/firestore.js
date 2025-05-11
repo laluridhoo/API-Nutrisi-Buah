9aebd35b3836c04b2f0fb57d23bfe771d93728c4
lalu kurnia muhammad ridho <nama_user@DESKTOP-SMPFNDV>
2025-05-11 22:11:17 +0800

    Menggunakan Application Default Credentials (ADC)

--- config/firestore.js
+++ config/firestore.js
@@ -1,28 +1,9 @@
 const admin = require('firebase-admin');
-const fs = require('fs');
 require('dotenv').config();
-
-const path = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
-let serviceAccount = null;
-if (path) {
-  try {
-    if (!fs.existsSync(path)) {
-      throw new Error(`File service account tidak ditemukan di path: ${path}`);
-    }
-    serviceAccount = JSON.parse(fs.readFileSync(path, 'utf8'));
-  } catch (err) {
-    console.error('Gagal membaca service account:', err.message);
-    process.exit(1);
-  }
-} else {
-  console.error('GOOGLE_APPLICATION_CREDENTIALS tidak terdefinisi di .env');
-  process.exit(1);
-}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
   });
 }
 
@@ -31,5 +12,4 @@ const db = admin.firestore();
 module.exports = {
   db,
   admin,
-  serviceAccount,
 };
