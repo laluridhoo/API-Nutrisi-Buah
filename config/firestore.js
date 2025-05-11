0df2422da480445fadbf666627d2ce180dd756f0
lalu kurnia muhammad ridho <nama_user@DESKTOP-SMPFNDV>
2025-05-11 16:25:32 +0800

    menambahkan foto diriwayat komsumsi harian

--- config/firestore.js
+++ config/firestore.js
@@ -1,8 +1,12 @@
 const admin = require('firebase-admin');
+const path = require('path');
+
+// Load credentials from file
+const serviceAccount = require(path.resolve('/home/nama_user/keyService/smart-nutrition-app-457602-f6d169feeb4d.json'));
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
@@ -11,5 +15,5 @@ const db = admin.firestore();
 module.exports = {
   db,
   admin,
-  serviceAccount: null, // kosongkan agar tidak error saat akses
+  serviceAccount, // jika ingin akses info credential
 };
