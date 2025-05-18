fd36bd7f024bbbf670697c85c3da8a5a358a3602
lalu kurnia muhammad ridho <nama_user@DESKTOP-SMPFNDV>
2025-05-18 16:57:20 +0800

    menangani ekstensi file gambar yang berbeda

--- server/cloudStorage.js
+++ server/cloudStorage.js
@@ -1,12 +1,22 @@
 // cloudStorage.js
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
-const bucketName = 'image-buah'; // Ganti dengan nama bucket kamu
+const bucketName = 'image-buah';
 
 const getImageUrlByFruitLabel = async (fruitLabel) => {
   if (!fruitLabel) return null;
-  const fileName = `${fruitLabel}.jpg`;
-  return `https://storage.googleapis.com/${bucketName}/${fileName}`;
+  const possibleExt = ['jpg', 'jpeg', 'png', 'webp'];
+  const bucket = storage.bucket(bucketName);
+
+  for (const ext of possibleExt) {
+    const fileName = `${fruitLabel}.${ext}`;
+    const file = bucket.file(fileName);
+    const [exists] = await file.exists();
+    if (exists) {
+      return `https://storage.googleapis.com/${bucketName}/${fileName}`;
+    }
+  }
+  return null; // Jika tidak ditemukan file dengan ekstensi apapun
 };
 
 module.exports = {
